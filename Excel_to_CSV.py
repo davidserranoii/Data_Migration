d62fa9775f942f87ed4522fb607becf54927927a
David Kent Serrano II <davidserranoii94@gmail.com>
2024-04-29 18:29:58 -0500

    Update Excel_to_CSV to be able to skip over files | Update Excel_to_CSV to use broad terms such as linking value instead of community name.

--- Excel_to_CSV.py
+++ Excel_to_CSV.py
@@ -17,17 +17,20 @@ for file in fileList:
     if file.endswith('.xlsx'):
         excelFile = pd.ExcelFile(data_file)
         sheetNames = excelFile.sheet_names
-        os.system('mkdir ' + '"' + selectFilePath + '\\' + str(file[:-5]) + '"')
-        communityName = input("What is the community name for " + str(file) + "?: ")
-        for sheet in sheetNames:
-            try:
-                df = excelFile.parse(sheet, dtype=str)
-                df.fillna("", inplace=True)
-                df = pd.DataFrame(df, dtype=str)
-                # print(df.head())
-                df.insert(loc=0, column='Community_Name', value=communityName)
-                df.to_csv((selectFilePath + '\\' + str(file[:-5]) + "\\" + str(sheet) + ".csv"), index=False)
-                print(str(sheet) + " loaded.")
-            except Exception as e:
-                print(e)
-                print(str(sheet) + 'failed to load in')
+        communityName = input("What is the linking vale for " + str(file) + "? 's' for skip: ")
+        if communityName == 's':
+            print(str(file) + " skipped.")
+        else:
+            os.system('mkdir ' + '"' + selectFilePath + '\\' + str(file[:-5]) + '"')
+            for sheet in sheetNames:
+                try:
+                    df = excelFile.parse(sheet, dtype=str)
+                    df.fillna("", inplace=True)
+                    df = pd.DataFrame(df, dtype=str)
+                    # print(df.head())
+                    df.insert(loc=0, column='Linking_Value', value=communityName)
+                    df.to_csv((selectFilePath + '\\' + str(file[:-5]) + "\\" + str(sheet) + ".csv"), index=False)
+                    print(str(sheet) + " loaded.")
+                except Exception as e:
+                    print(e)
+                    print(str(sheet) + 'failed to load in')
